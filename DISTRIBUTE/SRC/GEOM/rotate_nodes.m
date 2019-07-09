2cea88e270ada8f4d1c1a5e00e5b4dcf03efe26d
Jing Rebecca LI <jingrebecca.li@inria.fr>
2019-07-09 09:20:28 +0200

    Making NeuronModule

--- DISTRIBUTE/SRC/GEOM/rotate_nodes.m
+++
@@ -1,9 +0,0 @@
-function [Mrot] = rotate_nodes(u1,u2)
-% Rotation matrix that rotates vector u1 to u2
-
-u = u1+u2;
-u = u/norm(u);
-theta = pi;
-ucross = cross(repmat(u,[1,3]),eye(3));
-Mrot = cos(theta)*eye(3)+sin(theta)*ucross+(1-cos(theta))*u*u';
-
--- DISTRIBUTE/SRC/TETGEN/tetGen/win64/a.lua
+++
@@ -1,6 +0,0 @@
--- Lua script.
-p=tetview:new()
-p:load_plc("C:/Users/Jing Rebecca LI/Dropbox/SpinDoctor/CODE_JRL/GUI+SRC/voxel.poly")
-rnd=glvCreate(0, 0, 500, 500, "TetView")
-p:plot(rnd)
-glvWait()
