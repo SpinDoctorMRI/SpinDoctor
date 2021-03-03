de1e0f139e9037ded434adaa976a7c47ac5ae234
Syver Døving Agdestein <syverda@icloud.com>
2021-03-03 14:03:59 +0100

    Remove old version

--- DISTRIBUTE/SRC/mygdirs.m
+++
@@ -1,7 +0,0 @@
-function [points_gdir] = mygdirs(n)
-% generate n directions evenly distributed in the x-y plan
-    angle = linspace(0, 2*pi, n+1);
-    angle = angle(1:end-1)';
-    points_gdir = [cos(angle),sin(angle),zeros(n,1)];
-end
-
