de0f237ca91fe450482e9e96ad575afb756747fc
AlexMcSD <alex.mc-sweeney-davis@inria.fr>
2024-11-28 17:45:30 +0100

    Added westin paper analysis template

---
+++ driver_camino_develop.m
@@ -0,0 +1,349 @@
+%DRIVER_CAMINO Solve BTPDE or MF for a sequence inputted from a camino file.
+%
+%   Provides a template to demonstrate how camino scheme sequences can be
+%   integrated into SpinDoctor
+%
+%   The user is advised to read the latest version
+%   from \url{https://github.com/jingrebeccali/SpinDoctor}
+
+% Add SpinDoctor
+addpath(genpath("src"));
+addpath(genpath("drivers_postprocess"));
+
+%% Define inputs
+
+% Get setup
+addpath(genpath("setups"));
+setup_camino_develop;
+
+%% Prepare simulation
+[setup, femesh, surfaces, cells]  = prepare_simulation(setup);
+
+nsequence = setup.nsequence;
+%% Perform experiments
+
+% Perform MF experiments
+% Perform Laplace eigendecomposition
+tic
+save_path = create_savepath(setup,"mf");save_path = sprintf("%s/cell",save_path);
+lap_eig = compute_laplace_eig(femesh, setup.pde, setup.mf,save_path);
+    
+% Compute MF magnetization
+mf = solve_mf(femesh, setup, lap_eig,save_path,false);
+toc
+
+disp("Camino sequence results are stored in mf");
+
+%% Plot Geometry
+do_plots = true;
+
+if do_plots
+    % Plot surface triangulation
+    plot_surface_triangulation(surfaces);
+    
+    % Plot the finite element mesh
+    plot_femesh(femesh, setup.pde.compartments);
+    % plot_femesh_everywhere(femesh, "");
+    
+    % Plot information about the geometry
+    plot_geometry_info(setup, femesh);
+end
+
+
+%% Plot Sequences
+% Plot sequence
+for iseq = 1%:nsequence
+    title_str = sprintf("Sequence %d of %d",iseq,setup.nsequence);
+    setup.gradient.sequences{iseq}.plot(title_str);
+end
+
+gamma = setup.gamma * 1e-6;
+
+% Study B_tensor
+for iseq=1%:nsequence
+    B_tensor = setup.gradient.sequences{iseq}.get_B_tensor(gamma);
+    fprintf("For sequence %d of %d:\n",iseq,setup.nsequence);
+    B_tensor
+    plot_tensor(B_tensor,sprintf("B tensor for sequence %d of %d",iseq, setup.nsequence));
+end
+
+
+%% Fitting Diffusion tensor
+% See Q-space trajectory imaging for multidimensional diffusion MRI of the
+% human brain, (2016) Westin
+
+% Note that for a fixed sequence, B is a rank 2 tensor and we also compute
+% $B^(\otimes 2)$, which is rank 4. However, we represent it by a rank two
+% tensor as is done in Westin 2016. This is done by represening B by a
+% vector (so rank 1) and then $B^(\otimes 2)$ by the outer product of this
+% vector. The representations are chosen to preserve the inner product of
+% tensors.
+
+%% Extract B-tensor data and signals
+
+% Map from symmetric rank 2 tensor to vector representation (see eq 7)
+B_vector = @(B) [B(1,1) B(2,2) B(3,3) sqrt(2)*B(1,2) sqrt(2)*B(1,3) sqrt(2)*B(2,3)];
+
+% Matrix of weights to represent rank 4 tensor by rank 2 tensor with
+% reduced degrees of freedom (see eq 11)
+W= [
+    1 1 1 sqrt(2) sqrt(2) sqrt(2);
+    1 1 1 sqrt(2) sqrt(2) sqrt(2);
+    1 1 1 sqrt(2) sqrt(2) sqrt(2);
+    sqrt(2) sqrt(2) sqrt(2) 2 2 2;
+    sqrt(2) sqrt(2) sqrt(2) 2 2 2;
+    sqrt(2) sqrt(2) sqrt(2) 2 2 2
+    ] ;
+% Indices of unique entries in $B^(\otimes 2)$.
+ind = [1 2 3 4 5 6 8 9 10 11 12  15 16 17 18 22 23 24 29 30 36];
+
+
+B_tensors = zeros(3,3,nsequence);
+b_vectors = zeros(6,nsequence);
+B2_tensors = zeros(21,nsequence);
+for iseq=1:nsequence
+    B= setup.gradient.sequences{iseq}.get_B_tensor(gamma);
+    B_tensors(:,:,iseq) = B;
+    b = B_vector(B);
+    B2 = b'*b;
+    symm = W.*B2;
+    B2_tensors(:,iseq) = reshape(symm(ind),[],1);
+    b_vectors(:,iseq) = b;
+end
+B_tensors = reshape(B_tensors,9,120);
+
+S = log(real(mf.signal)./femesh.total_volume)';
+
+
+
+%% (Diffusion tensor) (EQ 12) Solve with Matlab
+% Set input variables
+X = b_vectors';
+
+% Solve equation, representing D,D^(\otimes 2) as a vector of size 30
+A = X\S;
+
+% Split A into D and D^(\otimes 2) vectors
+d = -A(1:6);
+
+% Diffusion matrix from vector representation
+Diffusion_matrix = @(d) [d(1) d(6)/sqrt(2) d(5)/sqrt(2); d(6)/sqrt(2) d(2) d(4)/sqrt(2); d(5)/sqrt(2) d(4)/sqrt(2) d(3)];
+D = Diffusion_matrix(d);
+rel_signal_error = abs(exp(X*A) - exp(S))./abs(exp(S));
+abs_signal_error = abs(exp(X*A) - exp(S));
+
+
+disp("Diffusion tensor");
+disp("Diffusion matrix = ");
+D
+disp("Eigenvalues = ");
+eig(D)
+
+fprintf("Mean relative signal error = %f, Std dev relative signal error = %f\n" + ...
+    "Mean absolute signal error = %f, Std dev absolute signal error = %f\n", ...
+    mean(rel_signal_error),std(rel_signal_error),mean(abs_signal_error),std(abs_signal_error));
+
+
+%% (Diffusion tensor and covariance) (EQ 13) Solve with Matlab
+% Set input variables
+X = [b_vectors',B2_tensors'];
+
+% Solve equation, representing D,D^(\otimes 2) as a vector of size 30
+A = X\S;
+
+% Split A into D and D^(\otimes 2) vectors
+d = -A(1:6);
+c = 2*A(7:27);
+
+% Diffusion matrix from vector representation
+Diffusion_matrix = @(d) [d(1) d(6)/sqrt(2) d(5)/sqrt(2); d(6)/sqrt(2) d(2) d(4)/sqrt(2); d(5)/sqrt(2) d(4)/sqrt(2) d(3)];
+D = Diffusion_matrix(d);
+C = Covariance_matrix(c);
+
+rel_signal_error = abs(exp(X*A) - exp(S))./abs(exp(S));
+abs_signal_error = abs(exp(X*A) - exp(S));
+
+disp("Diffusion tensor with covariance tensor")
+disp("Diffusion tensor = ")
+D
+disp("Eigenvalues = ");
+eig(D)
+disp("Diffusion covariance tensor = ")
+C
+disp("Eigenvalues = ");
+eig(C)
+
+fprintf("Mean relative signal error = %f, Std dev relative signal error = %f\n" + ...
+    "Mean absolute signal error = %f, Std dev absolute signal error = %f\n", ...
+    mean(rel_signal_error),std(rel_signal_error),mean(abs_signal_error),std(abs_signal_error));
+
+%% Misc variables for below
+G1 = [1 0 0 0 0 0; 0 0 0 0 0 1/sqrt(2); 0 0 0 0 1/sqrt(2) 0];
+G2 = [0 0 0 0 0 1/sqrt(2); 0 1 0 0 0 0; 0 0 0 1/sqrt(2) 0 0];
+G3 = [0 0 0 0 1/sqrt(2) 0; 0 0 0 1/sqrt(2) 0 0; 0 0 1 0 0 0];
+
+disp("User needs to set path to cvx here.")
+cd ../cvx
+cvx_setup
+cd ../SpinDoctor
+
+%% (Diffusion tensor) (EQ 12) Solve with cvx
+
+X = b_vectors';
+S = log(real(mf.signal)./femesh.total_volume)';
+clear d_cvx;
+cvx_begin
+    variable d_cvx(6);
+    minimize(norm( S - X*d_cvx));
+    subject to
+        [G1*d_cvx G2*d_cvx G3*d_cvx] == semidefinite(3);
+cvx_end
+
+D_cvx = Diffusion_matrix(d_cvx);
+disp("Diffusion tensor with cvx");
+disp("Diffusion matrix = ");
+D_cvx
+disp("Eigenvalues = ");
+eig(D_cvx)
+
+rel_signal_error = abs(exp(X*d_cvx) - exp(S))./abs(exp(S));
+abs_signal_error = abs(exp(X*d_cvx) - exp(S));
+
+fprintf("Mean relative signal error = %f, Std dev relative signal error = %f\n" + ...
+    "Mean absolute signal error = %f, Std dev absolute signal error = %f\n", ...
+    mean(rel_signal_error),std(rel_signal_error),mean(abs_signal_error),std(abs_signal_error));
+
+%% (Diffusion tensor and covariance) (EQ 13) Solve with cvx
+
+X = [b_vectors',B2_tensors'];
+S = log(real(mf.signal)./femesh.total_volume)';
+clear d_cvx c_cvx;
+cvx_begin
+    variable d_cvx(6);
+    variable c_cvx(21);
+    minimize(norm( S - X*[d_cvx' c_cvx']'));
+    subject to
+        [G1*d_cvx G2*d_cvx G3*d_cvx] == semidefinite(3);
+        Covariance_matrix(c_cvx) == semidefinite(6);
+cvx_end
+
+D_cvx = Diffusion_matrix(d_cvx);
+C_cvx = Covariance_matrix(c_cvx)
+disp("Diffusion tensor with cvx");
+disp("Diffusion matrix = ");
+D_cvx
+disp("Eigenvalues = ");
+eig(D_cvx)
+disp("Diffusion covariance tensor = ")
+C_cvx
+disp("Eigenvalues = ");
+eig(C_cvx)
+rel_signal_error = abs(exp(X*[d_cvx' c_cvx']') - exp(S))./abs(exp(S));
+abs_signal_error = abs(exp(X*[d_cvx' c_cvx']') - exp(S));
+
+
+fprintf("Mean relative signal error = %f, Std dev relative signal error = %f\n" + ...
+    "Mean absolute signal error = %f, Std dev absolute signal error = %f\n", ...
+    mean(rel_signal_error),std(rel_signal_error),mean(abs_signal_error),std(abs_signal_error));
+
+%% Compute biological parameters
+% Note I use Dmean2 to indicate $< D >^{\otimes 2}$ and 
+%            D2mean to indicate $< D^{\otimes 2} >$
+
+% Need to ensure that bounds are met by model.
+
+C = C_cvx;
+
+% Equations 16-19
+E_iso  = 1/3 * eye(6);
+E_bulk = 1/9*[ones(3,3) zeros(3,3);zeros(3,3) zeros(3,3)];
+E_shear = E_iso - E_bulk;
+V_md = dot(C(:),E_bulk(:));
+
+% Equation 6
+Dmean2 =squeeze(tensorprod(d,d));
+
+D2mean = C + Dmean2;
+
+% Equation 28 
+C_mu = 1.5* dot(D2mean(:),E_shear(:))/dot(D2mean(:),E_iso(:));
+% Equation 29 microscopic anistropy
+mu_FA = sqrt(C_mu);
+
+% Equation 30
+C_M = 1.5* dot(Dmean2(:),E_shear(:))/dot(Dmean2(:),E_iso(:));
+% Equation 30 macroscopic anistropy
+FA = sqrt(C_M);
+
+% Equation 32 orientation parameter
+OP = sqrt( dot(Dmean2(:),E_shear(:))/dot(D2mean(:),E_shear(:)));
+
+% Equation 33 microscopic orientation coherence
+C_c = C_M/C_mu;
+
+% Equation 41 bulk kurtosis
+K_bulk = 3*dot(C(:),E_bulk(:))/dot(Dmean2(:),E_bulk(:));
+
+% Equation 42 shear kurtosis
+K_shear= (6/5)*dot(C(:),E_shear(:))/dot(Dmean2(:),E_bulk(:));
+
+% Equation 43 microscopic kurtosis
+K_mu= (6/5)*dot(D2mean(:),E_shear(:))/dot(Dmean2(:),E_bulk(:));
+
+
+fprintf("microscopic anisotropy = %f\n", mu_FA);
+fprintf("macroscopic anisotropy = %f\n", FA);
+fprintf("orientation parameter = %f\n",OP );
+fprintf("bulk kurtosis = %f\n", K_bulk);
+fprintf("shear kurtosis = %f\n", K_shear);
+fprintf("microscopic kurtosis = %f\n", K_mu);
+
+fprintf("Bounds model should have:\n")
+fprintf("microscopic anisotropy in [0,1]\n");
+fprintf("orientation parameter in [0,1]\n");
+fprintf("May also be more, double check paper\n")
+
+%% Functions
+
+function C = Covariance_matrix(c)
+C = [
+    c(1) c(2) c(3) sqrt(2)*c(4) sqrt(2)*c(5) sqrt(2)*c(6);
+    c(2) c(7) c(8) sqrt(2)*c(9) sqrt(2)*c(10) sqrt(2)*c(11);
+    c(3) c(8) c(12) sqrt(2)*c(13) sqrt(2)*c(14) sqrt(2)*c(15);
+    sqrt(2)*c(4) sqrt(2)*c(9) sqrt(2)*c(13) 2*c(16) 2*c(17) 2*c(18);
+    sqrt(2)*c(5) sqrt(2)*c(10) sqrt(2)*c(14) 2*c(17) 2*c(19) 2*c(20);
+    sqrt(2)*c(6) sqrt(2)*c(11) sqrt(2)*c(15) 2*c(18) 2*c(20) 2*c(21)
+];
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ setups/setup_camino_develop.m
@@ -0,0 +1,101 @@
+%SETUP_NEURON Define setup structure for SpinDoctor.
+%
+%   The setup structure may contain the following substructures:
+%
+%       geometry:
+%           Geometry parameters. Can contain parameters for specific cell types
+%           (spheres, cylinders). Determines whether to include inner and ECS
+%           compartments), and finite element mesh parameters.
+%   
+%       pde:
+%           Domain parameters. Contains PDE parameters (material properties)
+%           defined for the domains "IN", "OUT", "ECS" and their boundaries
+%   
+%       gradient:
+%           Gradient sequence parameters. It determines the three properties
+%           `directions`, `amplitudes` and `sequences`. Available sequences are:
+%               PGSE(delta, Delta)
+%               DoublePGSE(delta, Delta)
+%               CosOGSE(delta, Delta, nperiod)
+%               SinOGSE(delta, Delta, nperiod)
+%               CustomSequence(delta, Delta, @timeprofile)
+%           The directions can be provided manually, or created by the functions
+%           `unitsphere`, `unitcircle`, or `unitsemicercle`.
+%
+%   The precense of any of the following substructures triggers the corresponding experiment:
+%
+%       btpde:          Solve Bloch-Torrey PDE with P1-FEM and provided ODE
+%                       solver
+%
+%       btpde_midpoint: Solve Bloch-Torrey PDE with P1-FEM and Crank-Nicolson
+%                       solver
+%
+%       hadc:           Solve the equation for the homogenized apparent
+%                       diffusion coefficient using P1-FEM
+%
+%       mf:             Compute the matrix formalism signal
+%
+%       analytical:     Compute analytical signal one analyticaled sphere or
+%                       cylinder using truncated radial matrix formalism
+%
+%       karger:         Solve for the finite pulse Karger model
+
+addpath(genpath('src'));
+%% File name to load or store cell description, surface geometry, mesh, and simulation results
+setup.name = "C:\Users\amcsween\SpinDoctor_mesh_files/spindle/whole_neurons/04b_spindle3aFI.ply";
+
+setup.saved_simul_loc = "C:\Users\amcsween\SpinDoctor_saved_simul";
+%% Geometry parameters
+setup.geometry.cell_shape = "neuron";                   % Cell shape; "sphere", "cylinder" or "neuron"
+setup.geometry.ncell = 1;                               % Number of cells
+setup.geometry.deformation = [0; 0];                    % Domain deformation; [a_bend,a_twist]
+setup.geometry.include_in = false;                      % Ratio Rin/R, within range [0,0.99]
+setup.geometry.in_ratio = 0.7;                          % Ratio Rin/R, within range [0,0.99]
+setup.geometry.ecs_shape = "no_ecs";                    % Shape of ECS: "no_ecs", "box", "convex_hull", or "tight_wrap".
+setup.geometry.ecs_ratio = 0.2;                         % ECS gap; percentage in side length
+
+% setup.geometry.refinement = 10;                       % Tetgen refinement parameter (comment for automatic)
+setup.geometry.tetgen_options = "-pq1.2a1.0O9VCn";              % Tetgen options (priority is inferior to refinement)
+
+%% PDE parameters
+setup.pde.diffusivity_in = 0.002;                       % Diffusion coefficient IN (scalar or 3x3-tensor)
+setup.pde.diffusivity_out = 0.002;                      % Diffusion coefficient OUT (scalar or 3x3-tensor)
+setup.pde.diffusivity_ecs = 0.002;                      % Diffusion coefficient ECS (scalar or 3x3-tensor)
+setup.pde.relaxation_in = Inf;                          % T2-relaxation IN. No relaxation: Inf
+setup.pde.relaxation_out = Inf;                         % T2-relaxation OUT. No relaxation: Inf
+setup.pde.relaxation_ecs = Inf;                         % T2-relaxtion ECS. No relaxation: Inf
+setup.pde.initial_density_in = 1.0;                     % Initial density in IN
+setup.pde.initial_density_out = 1.0;                    % Initial density in OUT
+setup.pde.initial_density_ecs = 1.0;                    % Initial density in ECS
+setup.pde.permeability_in_out = 1e-4;                   % Permeability IN-OUT interface
+setup.pde.permeability_out_ecs = 1e-4;                  % Permeability OUT-ECS interface
+setup.pde.permeability_in = 0;                          % Permeability IN boundary
+setup.pde.permeability_out = 0;                         % Permeability OUT boundary
+setup.pde.permeability_ecs = 0;                         % Permeability ECS boundary
+
+%% Gradient sequences
+seq = read_scheme("camino_sequences/morez_Q3_camino.scheme");
+% if isfile('camino_sequences/test_scheme.txt')           % Load tensors, unneccessary for simulations
+%     b_tensors = read_b_tensors('camino_sequences/test_scheme.txt');
+%     for i = 1:length(seq)
+%         seq{i}.b_tensor_axis_symmetric = b_tensors(:,i);
+%     end
+% end
+setup.gradient.sequences =seq;
+%% BTPDE midpoint experiment parameters (comment block to skip experiment)
+setup.btpde_midpoint.implicitness = 0.5;                % Theta-parameter: 0.5 for Crank-Nicolson
+setup.btpde_midpoint.timestep = 2;                      % Time step dt
+setup.btpde.rerun = true;    
+%% MF experiment parameters (comment block to skip experiment)
+% Length scale hard-coded for these experiments from the diffusivity values and sequence length.
+char_length_scale = sqrt(2*3*setup.pde.diffusivity_in*1000*101);
+setup.mf.length_scale =char_length_scale/5;             % Minimum length scale of eigenfunctions
+setup.mf.neig_max = 1000;                               % Requested number of eigenvalues
+setup.mf.ninterval = 200;                               % Number of intervals to discretize time profile in MF (if not PGSE and doublePGSE)
+setup.mf.eigs.sigma = 1e-8;
+setup.mf.rerun = false;
+%% BTPDE experiment parameters (comment block to skip experiment)
+setup.btpde.ode_solver = @ode15s;                       % ODE solver for BTPDE
+setup.btpde.reltol = 1e-4;                              % Relative tolerance for ODE solver
+setup.btpde.abstol = 1e-6;                              % Absolute tolerance for ODE solver
+setup.btpde.rerun = false;                               % Rerun simulation with or without saved results
